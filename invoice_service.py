e2c045b37bbb4c55da76f1b0b39eebf979441dee
Katarzyna Kraik <kasia.kraik@gmail.com>
2026-07-31 20:45:53 +0200

    feat: implement invoice analysis for local files

---
+++ invoice_service.py
@@ -0,0 +1,12 @@
+from azure_client import create_document_intelligence_client
+from azure.ai.documentintelligence.models import AnalyzeResult
+
+
+def analyze_invoice_from_file(file_path: str) -> AnalyzeResult:
+    client = create_document_intelligence_client()
+
+    with open(file_path, "rb") as file:
+        poller = client.begin_analyze_document(model_id="prebuilt-invoice", body=file)
+
+    result = poller.result()
+    return result
