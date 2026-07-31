f3e6d9a3a66394c7354b85d20bbe25412ce18f78
Katarzyna Kraik <kasia.kraik@gmail.com>
2026-07-31 20:53:15 +0200

    feat: implement invoice analysis from URL source

--- invoice_service.py
+++ invoice_service.py
@@ -1,5 +1,5 @@
 from azure_client import create_document_intelligence_client
-from azure.ai.documentintelligence.models import AnalyzeResult
+from azure.ai.documentintelligence.models import AnalyzeResult, AnalyzeDocumentRequest
 
 
 def analyze_invoice_from_file(file_path: str) -> AnalyzeResult:
@@ -8,5 +8,14 @@ def analyze_invoice_from_file(file_path: str) -> AnalyzeResult:
     with open(file_path, "rb") as file:
         poller = client.begin_analyze_document(model_id="prebuilt-invoice", body=file)
 
+    result = poller.result()
+    return result
+
+
+def analyze_invoice_from_url(file_url: str) -> AnalyzeResult:
+    client = create_document_intelligence_client()
+
+    poller = client.begin_analyze_document(model_id="prebuilt-invoice", body=AnalyzeDocumentRequest(url_source=file_url))
+    
     result = poller.result()
     return result
